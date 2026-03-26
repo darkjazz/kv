30950fd6b36a435b2a1f2e6a181e23356337dae8
darkjazz <nothing@tehis.net>
2026-03-26 19:26:42 +0000

    rename pool osc to /lambda/liquid/*

--- src/osc.cpp
+++ src/osc.cpp
@@ -524,13 +524,13 @@ void OSCMessenger::setUpListener() {
     });
 
     // Standalone water scene (pool + caustics, evanw approach)
-    _listener.setListener( "/lambda/pool/show",
+    _listener.setListener( "/lambda/liquid/show",
     [&]( const osc::Message &msg ){
         bool on = msg.getArgInt32(0) == 1;
         _ogl->mWaterScene.isVisible = on;
         console() << "WaterScene: " << (on ? "visible" : "hidden") << std::endl;
     });
-    _listener.setListener( "/lambda/pool/drop",
+    _listener.setListener( "/lambda/liquid/drop",
     [&]( const osc::Message &msg ){
         // args: worldX worldZ radius strength
         float x = msg.getArgFloat(0);
@@ -539,38 +539,38 @@ void OSCMessenger::setUpListener() {
         float s = msg.getArgFloat(3);
         _ogl->mWaterScene.addDrop(x, z, r, s);
     });
-    _listener.setListener( "/lambda/pool/cymatics",
+    _listener.setListener( "/lambda/liquid/cymatics",
     [&]( const osc::Message &msg ){
         _ogl->mWaterScene.cymatics = msg.getArgInt32(0) == 1;
     });
-    _listener.setListener( "/lambda/pool/damping",
+    _listener.setListener( "/lambda/liquid/damping",
     [&]( const osc::Message &msg ){
         _ogl->mWaterScene.damping = msg.getArgFloat(0);
     });
-    _listener.setListener( "/lambda/pool/speed",
+    _listener.setListener( "/lambda/liquid/speed",
     [&]( const osc::Message &msg ){
         _ogl->mWaterScene.waveSpeed = msg.getArgFloat(0);
     });
-    _listener.setListener( "/lambda/pool/caustic",
+    _listener.setListener( "/lambda/liquid/caustic",
     [&]( const osc::Message &msg ){
         // args: strength scale
         _ogl->mWaterScene.causticStrength = msg.getArgFloat(0);
         if (msg.getNumArgs() > 1) _ogl->mWaterScene.causticScale = msg.getArgFloat(1);
     });
-    _listener.setListener( "/lambda/pool/surface",
+    _listener.setListener( "/lambda/liquid/surface",
     [&]( const osc::Message &msg ){
         // args: show(0|1) alpha heightScale
         _ogl->mWaterScene.drawSurface = msg.getArgInt32(0) == 1;
         if (msg.getNumArgs() > 1) _ogl->mWaterScene.waterAlpha  = msg.getArgFloat(1);
         if (msg.getNumArgs() > 2) _ogl->mWaterScene.heightScale = msg.getArgFloat(2);
     });
-    _listener.setListener( "/lambda/pool/color",
+    _listener.setListener( "/lambda/liquid/color",
     [&]( const osc::Message &msg ){
         // args: r g b  (pool floor/wall base color)
         _ogl->mWaterScene.poolColor = vec3(
             msg.getArgFloat(0), msg.getArgFloat(1), msg.getArgFloat(2));
     });
-    _listener.setListener( "/lambda/pool/watercolor",
+    _listener.setListener( "/lambda/liquid/watercolor",
     [&]( const osc::Message &msg ){
         // args: r g b
         _ogl->mWaterScene.waterColor = vec3(
